29909585a311ab37e2f61e77b7c9812a88ff0352
wilson <yizhen_wzone@163.com>
2021-07-09 12:14:33 +0800

    删除了一些不必要的空格
    无关痛痒的调整

--- chapter6-实战指南/config.py
+++ chapter6-实战指南/config.py
@@ -2,9 +2,10 @@
 import warnings
 import torch as t
 
+
 class DefaultConfig(object):
     env = 'default'  # visdom 环境
-    vis_port =8097 # visdom 端口
+    vis_port = 8097  # visdom 端口
     model = 'SqueezeNet'  # 使用的模型，名字必须与models/__init__.py中的名字一致
 
     train_data_root = './data/train/'  # 训练集存放路径
@@ -24,7 +25,6 @@ class DefaultConfig(object):
     lr_decay = 0.5  # when val_loss increase, lr = lr*lr_decay
     weight_decay = 0e-5  # 损失函数
 
-
     def _parse(self, kwargs):
         """
         根据字典kwargs 更新 config参数
@@ -33,13 +33,13 @@ class DefaultConfig(object):
             if not hasattr(self, k):
                 warnings.warn("Warning: opt has not attribut %s" % k)
             setattr(self, k, v)
-        
-        opt.device =t.device('cuda') if opt.use_gpu else t.device('cpu')
 
+        opt.device = t.device('cuda') if opt.use_gpu else t.device('cpu')
 
         print('user config:')
         for k, v in self.__class__.__dict__.items():
             if not k.startswith('_'):
                 print(k, getattr(self, k))
+
 
 opt = DefaultConfig()
